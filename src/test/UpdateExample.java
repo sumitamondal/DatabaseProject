08fcb821e1a4da15b66abdaabc95bd7437b84cb3
Sumitamondal <sumita.prabhatmondal@gmail.com>
2022-08-06 06:30:26 +0000

    First commit

---
+++ src/test/UpdateExample.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateExample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+
+        Class.forName("com.mysql.cj.jdbc.Driver");  
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "root");
+		
+		Statement stm = con.createStatement();
+		
+		stm.executeUpdate("update eproduct set price=2500.00 where name='Tablet'");
+
+		ResultSet result = stm.executeQuery("select * from eproduct");
+		
+		while(result.next()) {
+			
+			System.out.print(result.getInt("ID") + " ");
+			System.out.print(result.getString("name")+ " ");
+			System.out.println(result.getFloat("price"));
+			
+	
+		}
+
+	}
+
+}
